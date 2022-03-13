d9c5fc746cc80867f29dce3d5c01691a637ce11f
Andre <andssouzza@gmail.com>
2022-03-13 19:18:20 -0400

    primeiro commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-function exibirSenha() {
-  let password = document.querySelector('.senha')
-  console.log(password.value)
+let password = document.querySelector('.senha')
+password.style.border = "1px solid #6DC68B"
 
+let icone = document.querySelector('.senhaIcone');
+icone.style.cursor = "pointer"
+
+function exibirSenha() {
   if(password.type == 'password') {
     password.type = 'text'
   }else {
